a8a62358272cb6ddb057fe25e3994a9eaa1b70c3
Michael de Radobýl <mjohne@users.noreply.github.com>
2025-09-19 07:28:04 +0200

    Add files via upload

---
+++ CrossOperations.cs
@@ -0,0 +1,134 @@
+﻿namespace DigitX
+{
+	internal class CrossOperations
+	{
+		// 1. private Digit Sum
+		public static int DigitSum(int number)
+		{
+			int sum = 0;
+			int n = Math.Abs(value: number);
+			while (n > 0)
+			{
+				sum += n % 10;
+				n /= 10;
+			}
+			return sum;
+		}
+
+		// 2. Digit Product
+		public static int DigitProduct(int number)
+		{
+			int product = 1;
+			int n = Math.Abs(value: number);
+			if (n == 0)
+			{
+				return 0;
+			}
+
+			while (n > 0)
+			{
+				product *= n % 10;
+				n /= 10;
+			}
+			return product;
+		}
+
+		// 3a. Alternating Digit Difference (from front/back)
+		public static int AlternatingDigitDifference(int number, bool fromFront = true)
+		{
+			int n = Math.Abs(value: number);
+			string text = n.ToString();
+			int difference = 0;
+
+			if (fromFront)
+			{
+				for (int i = 0; i < text.Length; i++)
+				{
+					int digit = text[index: i] - '0';
+					difference += (i % 2 == 0) ? digit : -digit;
+				}
+			}
+			else
+			{
+				bool plus = true;
+				for (int i = text.Length - 1; i >= 0; i--)
+				{
+					int digit = text[index: i] - '0';
+					difference += plus ? digit : -digit;
+					plus = !plus;
+				}
+			}
+
+			return difference;
+		}
+
+		// 3b. Simple Digit Difference (subtract all, from front/back)
+		public static int SimpleDigitDifference(int number, bool fromFront = true)
+		{
+			int n = Math.Abs(value: number);
+			string text = n.ToString();
+			if (text.Length == 0)
+			{
+				return 0;
+			}
+
+			int difference;
+			if (fromFront)
+			{
+				difference = text[index: 0] - '0';
+				for (int i = 1; i < text.Length; i++)
+				{
+					difference -= text[index: i] - '0';
+				}
+			}
+			else
+			{
+				difference = text[^1] - '0';
+				for (int i = text.Length - 2; i >= 0; i--)
+				{
+					difference -= text[index: i] - '0';
+				}
+			}
+			return difference;
+		}
+
+		// 4. Safe Digit Quotient (divide, skip zeros, from front/back)
+		public static double SafeDigitQuotient(int number, bool fromFront = true)
+		{
+			int n = Math.Abs(value: number);
+			string text = n.ToString();
+			if (text.Length == 0)
+			{
+				return 0;
+			}
+
+			double quotient;
+			if (fromFront)
+			{
+				quotient = text[index: 0] - '0';
+				for (int i = 1; i < text.Length; i++)
+				{
+					int digit = text[index: i] - '0';
+					if (digit != 0)
+					{
+						quotient /= digit;
+					}
+				}
+			}
+			else
+			{
+				quotient = text[^1] - '0';
+				for (int i = text.Length - 2; i >= 0; i--)
+				{
+					int digit = text[index: i] - '0';
+					if (digit != 0)
+					{
+						quotient /= digit;
+					}
+				}
+			}
+
+			return quotient;
+		}
+	}
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DigitX
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.labelNumber = new Label();
+			this.numericUpDownDigitSum = new NumericUpDown();
+			this.labelDigitSum = new Label();
+			((System.ComponentModel.ISupportInitialize)this.numericUpDownDigitSum).BeginInit();
+			this.SuspendLayout();
+			// 
+			// labelNumber
+			// 
+			this.labelNumber.AutoSize = true;
+			this.labelNumber.Location = new Point(12, 14);
+			this.labelNumber.Name = "labelNumber";
+			this.labelNumber.Size = new Size(49, 15);
+			this.labelNumber.TabIndex = 0;
+			this.labelNumber.Text = "number";
+			// 
+			// numericUpDownDigitSum
+			// 
+			this.numericUpDownDigitSum.Location = new Point(67, 12);
+			this.numericUpDownDigitSum.Maximum = new decimal(new int[] { int.MaxValue, 0, 0, 0 });
+			this.numericUpDownDigitSum.Name = "numericUpDownDigitSum";
+			this.numericUpDownDigitSum.Size = new Size(120, 23);
+			this.numericUpDownDigitSum.TabIndex = 1;
+			this.numericUpDownDigitSum.TextAlign = HorizontalAlignment.Center;
+			this.numericUpDownDigitSum.ValueChanged += this.NumericUpDownDigitSum_ValueChanged;
+			// 
+			// labelDigitSum
+			// 
+			this.labelDigitSum.AutoSize = true;
+			this.labelDigitSum.Location = new Point(12, 53);
+			this.labelDigitSum.Name = "labelDigitSum";
+			this.labelDigitSum.Size = new Size(60, 15);
+			this.labelDigitSum.TabIndex = 2;
+			this.labelDigitSum.Text = "digit sum:";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new SizeF(7F, 15F);
+			this.AutoScaleMode = AutoScaleMode.Font;
+			this.ClientSize = new Size(207, 245);
+			this.Controls.Add(this.labelDigitSum);
+			this.Controls.Add(this.numericUpDownDigitSum);
+			this.Controls.Add(this.labelNumber);
+			this.FormBorderStyle = FormBorderStyle.FixedSingle;
+			this.Name = "MainForm";
+			this.StartPosition = FormStartPosition.CenterScreen;
+			this.Text = "DigitX";
+			((System.ComponentModel.ISupportInitialize)this.numericUpDownDigitSum).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+
+		#endregion
+
+		private Label labelNumber;
+		private NumericUpDown numericUpDownDigitSum;
+		private Label labelDigitSum;
+	}
+}
